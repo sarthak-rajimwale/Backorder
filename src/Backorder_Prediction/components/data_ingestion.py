e57a219c7ec84d0295fc4f871ff05e122b8250cc
sarthak-rajimwale <sarthakrajimwale98@gmail.com>
2024-10-21 16:35:41 +0530

    dvc ingestion

--- src/Backorder_Prediction/components/data_ingestion.py
+++ src/Backorder_Prediction/components/data_ingestion.py
@@ -12,9 +12,6 @@ class DataIngestionConfig:
     """Configuration for data ingestion, defining paths for saving the data."""
     train_data_path: str = os.path.join('artifacts', 'train.csv')
     test_data_path: str = os.path.join('artifacts', 'test.csv')
-    raw_train_data_path: str = os.path.join('artifacts', 'raw_train.csv')
-    raw_test_data_path: str = os.path.join('artifacts', 'raw_test.csv')
-
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
@@ -31,11 +28,9 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             # Save the raw and processed training data to CSV files
-            df_train.to_csv(self.ingestion_config.raw_train_data_path, index=False, header=True)
             df_train.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
 
             # Save the raw and processed test data to CSV files
-            df_test.to_csv(self.ingestion_config.raw_test_data_path, index=False, header=True)
             df_test.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Data Ingestion completed successfully")
